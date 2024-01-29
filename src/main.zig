a589b6ce1edd0fabebf7815e63edc71f1bd4dac4
Vojtěch Javora <vojta.javora@gmail.com>
2024-01-29 11:46:12 +0100

    Better error handeling. Removing panics

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,16 @@
 const std = @import("std");
 
-pub fn main() !void {
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+const BF_MEMORY = 0x8000;
+
+// TODO: think about debug mode
+
+const SUCCESS = 0;
+const FAILURE = 255;
+
+pub fn main() u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() == .leak) {
         std.log.err("Leak detected!", .{});
@@ -8,24 +18,63 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     const source: []const u8 = blk: {
-        var args = try std.process.argsWithAllocator(allocator);
+        var args = std.process.argsWithAllocator(allocator) catch @panic("OOM");
         defer args.deinit();
 
         _ = args.skip(); // argv[0]
         var file = if (args.next()) |filename|
-            try std.fs.cwd().openFile(filename, .{})
+            std.fs.cwd().openFile(filename, .{}) catch |err| {
+                std.log.err("File open failed: {s}", .{@errorName(err)});
+                return FAILURE;
+            }
         else
             std.io.getStdIn();
         defer file.close();
 
-        break :blk try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+        break :blk file.readToEndAlloc(
+            allocator,
+            std.math.maxInt(usize),
+        ) catch |err| {
+            std.log.err("File read failed: {s}", .{@errorName(err)});
+            return FAILURE;
+        };
     };
     defer allocator.free(source);
 
-    var bf = BfMachine.init(allocator, 0x8000);
+    var compile_diag: BFIR.Diagnostics = undefined;
+    const compiled_source = BFIR.compile(
+        source,
+        allocator,
+        &compile_diag,
+    ) catch |err| switch (err) {
+        BFIR.Error.OutOfMemory => {
+            std.log.err("{s}", .{@errorName(err)});
+            return FAILURE;
+        },
+        BFIR.Error.compileError => {
+            std.log.scoped(.compile).err(
+                "{s} at {}",
+                .{ compile_diag.msg, compile_diag.byte_offset },
+            );
+            return FAILURE;
+        },
+    };
+    defer allocator.free(compiled_source);
+
+    var bf = BfMachine.init(allocator, BF_MEMORY) catch |err| {
+        std.log.err("{s}", .{@errorName(err)});
+        std.os.exit(FAILURE);
+    };
     defer bf.deinit();
 
-    bf.run(source);
+    bf.run(compiled_source, true) catch |err| switch (err) {
+        BfMachine.Error.tapeOverrun => {
+            std.log.scoped(.runtime).err("tape overrun", .{});
+            return FAILURE;
+        },
+    };
+
+    return SUCCESS;
 }
 
 const BfMachine = struct {
@@ -34,10 +83,13 @@ const BfMachine = struct {
     ptr: usize = 0,
     mem: []u8 = undefined,
 
-    pub fn init(allocator: std.mem.Allocator, tape_length: usize) BfMachine {
+    pub fn init(
+        allocator: std.mem.Allocator,
+        tape_length: usize,
+    ) Allocator.Error!BfMachine {
         return .{
             .allocator = allocator,
-            .mem = allocator.alloc(u8, tape_length) catch @panic("OOM"),
+            .mem = try allocator.alloc(u8, tape_length),
         };
     }
 
@@ -45,30 +97,38 @@ const BfMachine = struct {
         self.allocator.free(self.mem);
     }
 
-    pub fn run(self: *BfMachine, src: []const u8) void {
-        @memset(self.mem, 0);
-        const compiled_source = BFIR.compile(src, self.allocator);
-        defer self.allocator.free(compiled_source);
+    // TODO: think about adding diagnostics
+    //  pros: error messages
+    //  cons: offset into compiled_source is useless. Also, I can just read pc
+    pub fn run(
+        self: *BfMachine,
+        compiled_source: []BFIR,
+        clear_memory: bool,
+    ) Error!void {
+        if (clear_memory) @memset(self.mem, 0);
 
         while (self.pc < compiled_source.len) {
-            self.execute(compiled_source[self.pc]);
+            try self.execute(compiled_source[self.pc]);
         }
     }
 
-    fn execute(self: *BfMachine, op: BFIR) void {
+    fn execute(self: *BfMachine, op: BFIR) Error!void {
         switch (op) {
             .ptr_inc => |n| {
-                if (self.ptr + n >= self.mem.len) @panic("Tape overflow");
+                if (self.ptr + n >= self.mem.len) return Error.tapeOverrun;
                 self.ptr += n;
             },
             .ptr_dec => |n| {
-                if (self.ptr < n) @panic("Tape underflow");
+                if (self.ptr < n) return Error.tapeOverrun;
                 self.ptr -= n;
             },
             .mem_inc => |n| self.mem[self.ptr] +%= @intCast(n % std.math.maxInt(u8)),
             .mem_dec => |n| self.mem[self.ptr] -%= @intCast(n % std.math.maxInt(u8)),
-            .out => _ = std.io.getStdOut().write(self.mem[self.ptr .. self.ptr + 1]) catch @panic("Write failed"),
-            .in => _ = std.io.getStdIn().read(self.mem[self.ptr .. self.ptr + 1]) catch @panic("Read failed"),
+            // TODO: buffering
+            .out => _ = std.io.getStdOut()
+                .write(self.mem[self.ptr .. self.ptr + 1]) catch @panic("Write failed"),
+            .in => _ = std.io.getStdIn()
+                .read(self.mem[self.ptr .. self.ptr + 1]) catch @panic("Read failed"),
             .left_b => |n| if (self.mem[self.ptr] == 0) {
                 self.pc = n;
             },
@@ -80,13 +140,20 @@ const BfMachine = struct {
         self.pc += 1;
     }
 
+    const Error = error{
+        tapeOverrun,
+    };
+
     fn debug_dump(self: BfMachine) void {
         std.debug.print("\n", .{});
         std.debug.print("REGS:\n", .{});
         std.debug.print("PC: {}\tPTR: {}\n", .{ self.pc, self.ptr });
         std.debug.print("MEMORY:\n", .{});
         std.debug.print("{any}\n", .{self.mem[self.ptr -| 10..self.ptr +| 10]});
-        std.debug.print("CURRENT VALUE: {} {c}\n", .{ self.mem[self.ptr], self.mem[self.ptr] });
+        std.debug.print("CURRENT VALUE: {} {c}\n", .{
+            self.mem[self.ptr],
+            self.mem[self.ptr],
+        });
     }
 };
 
@@ -102,12 +169,18 @@ const BFIR = union(enum) {
     right_b: usize,
     debug,
 
-    fn compile(source: []const u8, allocator: std.mem.Allocator) []BFIR {
-        var ir = std.ArrayList(BFIR).init(allocator);
+    fn compile(
+        source: []const u8,
+        allocator: std.mem.Allocator,
+        diag: *Diagnostics,
+    ) Error![]BFIR {
+        var ir = ArrayList(BFIR).init(allocator);
         errdefer ir.deinit();
+        try ir.ensureTotalCapacity(source.len);
 
-        var bracket_stack = std.ArrayList(usize).init(allocator);
+        var bracket_stack = ArrayList(usize).init(allocator);
         defer bracket_stack.deinit();
+        try bracket_stack.ensureTotalCapacity(source.len);
 
         var i: usize = 0;
         while (i < source.len) : (i += 1) {
@@ -116,33 +189,46 @@ const BFIR = union(enum) {
                 inline '>', '<', '+', '-' => |c| blk: {
                     const n = consumeWhile(source[i..], op);
                     i += n - 1;
-                    break :blk char_to_op(c, n);
+                    break :blk charToOp(c, n);
                 },
                 '.' => .out,
                 ',' => .in,
                 '#' => .debug,
                 '[' => blk: {
-                    bracket_stack.append(ir.items.len) catch |err| @panic(@errorName(err));
+                    bracket_stack.appendAssumeCapacity(ir.items.len);
                     break :blk .{ .left_b = undefined };
                 },
                 ']' => blk: {
-                    const left = bracket_stack.popOrNull() orelse
-                        std.debug.panic("Unmatched bracket at {}", .{i});
+                    const left = bracket_stack.popOrNull() orelse {
+                        diag.byte_offset = i;
+                        diag.msg = "Unmatched ']'";
+                        return Error.compileError;
+                    };
                     ir.items[left].left_b = ir.items.len;
                     break :blk .{ .right_b = left };
                 },
                 else => continue,
             };
 
-            ir.append(ir_op) catch |err| @panic(@errorName(err));
+            ir.appendAssumeCapacity(ir_op);
         }
 
-        if (bracket_stack.items.len != 0) @panic("Unmatched left brackets remaining");
-
-        return ir.toOwnedSlice() catch |err| @panic(@errorName(err));
+        if (bracket_stack.items.len != 0) {
+            diag.byte_offset = i;
+            diag.msg = "Unmatched '[' remaining";
+            return error.compileError;
+        }
+        return try ir.toOwnedSlice();
     }
 
-    fn char_to_op(comptime c: u8, n: usize) BFIR {
+    const Diagnostics = struct {
+        byte_offset: usize,
+        msg: []const u8,
+    };
+
+    const Error = error{compileError} || Allocator.Error;
+
+    fn charToOp(comptime c: u8, n: usize) BFIR {
         const field = switch (c) {
             '>' => "ptr_inc",
             '<' => "ptr_dec",
